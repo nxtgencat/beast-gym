fee78008781712fbc2908046f2f937b94810ac5e
nxtgencat <thesushanthmail@gmail.com>
2026-07-20 16:18:35 +0530

    meow

--- src/data/facilities.ts
+++ src/data/facilities.ts
@@ -4,5 +4,5 @@ export const facilities: Facility[] = [
   { img: 'public/assets/cardio.png', alt: 'Cardio zone with treadmills', label: 'Cardio Zone' },
   { img: 'public/assets/exercise.png', alt: 'Exercise studio with people training', label: 'Excercise Studio' },
   { img: 'public/assets/weight.png', alt: 'Woman weight training with dumbbells', label: 'Weight Training' },
-  { img: 'public/assets/function.png', alt: 'Functional training zone', label: 'Functional Zone' },
+  { img: 'public/assets/functional.png', alt: 'Functional training zone', label: 'Functional Zone' },
 ];
